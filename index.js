f767c5cc753c87b01de538f0b85ee0127792fc33
Billy Clinton Mabunda <mclintonsa@gmail.com>
2024-03-22 19:38:37 +0200

    Amended ternary operators

--- index.js
+++ index.js
@@ -5,14 +5,16 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+let result = userPreference = 'Regular' ? 'Regular' : 'Sugar-free'
+console.log(result);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+let message = cansLeft < 5 ? "Time to restock!" : "We're stocked!";
+console.log(message);
 
 
 // #### Challenge 3: Workout Intensity
@@ -20,7 +22,8 @@ console.log(``);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
+let outcome = heartRate < 100 ? "Boost needed!" : "Energy levels are high!";
+console.log(outcome);
 
 
 // #### Challenge 4: Temperature Suitability
@@ -28,7 +31,8 @@ console.log(`'}`);
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
+let output = currentTemp <= 5 ? "Chilled to perfection!" : "Needs a cooler!";
+console.log(output);
 
 
 // #### Challenge 5: Late Night Coding Session
@@ -38,12 +42,8 @@ console.log(``);
 
 // Define the current hour in 24-hour format
 let currentHour = 22; 
-
-// Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
-// Otherwise, print "Better stick to water."
-
-
+let comment = currentHour >= 7 && currentHour < 24 ? "Unleash the beast!" : "Better stick to water.";
+console.log(comment);
 
 
 
